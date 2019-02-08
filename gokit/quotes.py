ea044fbf0c9794d432a4319a4f60235fbafd68e6
Sridhar N <neelamraju.sridhar@gmail.com>
2019-02-08 16:24:31 +0000

    rm

--- gokit/quotes.py
+++
@@ -1,6 +0,0 @@
-
-
-
-class Vivekananda(object):
-    def quote_dict(self):
-        vvk_dict={'0':['We are what our thoughts have made us; so take care about what you think. Words are secondary. Thoughts live; they travel far.--Vivekananda']}
