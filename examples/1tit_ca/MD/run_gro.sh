844ef3b2baf36eaafb763eb3b51ad973ac665988
Sridhar N <neelamraju.sridhar@gmail.com>
2019-03-04 19:30:21 +0000

    Added 1tit_ca

--- examples/1tit_ca/MD/run_gro.sh
+++
@@ -1,3 +0,0 @@
-
-grompp -f md.mdp -c gromacs.gro -p gromacs.top -po mdout.out -o run.tpr -maxwarn 1 > a 2>b
-mdrun -x traj.xtc -e ener.edr -o traj.trr -s run.tpr -g md.log -table table_file.xvg -tablep table_file.xvg > run.out 2> run.err
