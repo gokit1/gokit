97285509369f9b01f652c10834c2cb5ea6b8a401
Sridhar N <neelamraju.sridhar@gmail.com>
2019-03-09 18:14:32 +0000

    fixed dsb MD example

--- examples/MDfiles/run_gro.sh
+++ examples/MDfiles/run_gro.sh
@@ -1,5 +1,2 @@
-gmx grompp -f md.mdp  -c gromacs.gro  -p gromacs.top  -po mdout.mdp -o run.tpr -maxwarn 1 > a 2> b
-
-
-
-gmx mdrun -x traj.xtc -e ener.edr -o traj.trr -s run.tpr -g md.log -table table_file.xvg > run.out 2> run.err
+gmx grompp -f md.mdp -c gromacs.gro -p gromacs.top -po mdout.mdp -o run.tpr -maxwarn 1
+gmx mdrun -x traj.xtc -e ener.edr -o traj.trr -s run.tpr -g md.log -table table_file.xvg 
--- examples/MDfiles/run_gro_dsb.sh
+++ examples/MDfiles/run_gro_dsb.sh
@@ -1,2 +1,2 @@
-gmx grompp -f md.mdp -c gromacs.gro -p gromacs.top -po mdout.mdp -o run.tpr -maxwarn 1
-gmx mdrun -x traj.xtc -e ener.edr -o traj.trr -s run.tpr -g md.log -table table_file.xvg -tableb table_files/table.xvg
+gmx grompp -f md.mdp -c gromacs.gro -p gromacs.top -po mdout.mdp -o run.tpr -maxwarn 1 
+gmx mdrun -x traj.xtc -e ener.edr -o traj.trr -s run.tpr -g md.log -tablep table_file.xvg  -table table_file.xvg -tableb table_files/table_b*.xvg  
